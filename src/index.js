84b76441e3e221a4ca6c6d04b0b5eedeadd8d631
Dustin Specker <dustinspecker@dustinspecker.com>
2015-08-30 12:40:17 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict';
+import cssColorNames from 'css-color-names';
+
+/**
+ * Determine if name is a valid CSS color name
+ * @param {String} name - name to determine if is valid CSS color name
+ * @return {Boolean} - is name a valid CSS color name?
+ */
+export default function isCSSColorName(name) {
+  return !!cssColorNames[name];
+}

8a50c2c31aa1c3f0aecbfda4133aae1812c292dc
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-12 19:10:30 -0600

    fix(src): use module.exports instead of default
    Babel 6 changes how default export interops with commonjs.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import cssColorNames from 'css-color-names';
  * @param {String} name - name to determine if is valid CSS color name
  * @return {Boolean} - is name a valid CSS color name?
  */
-export default function isCSSColorName(name) {
+module.exports = function (name) {
   return typeof name === 'string' ? !!cssColorNames[name.toLowerCase()] : false;
-}
+};
